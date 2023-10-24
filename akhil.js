78daf37acdb59d7d1bdbbfce6a13a68c3299375e
RhythmusByte <akhilmahesh@proton.me>
2023-10-24 20:04:50 +0530

    Added comments and an alert

--- akhil.js
+++ akhil.js
@@ -1,5 +1,13 @@
+// Declare Variables
+
 let name = 'RhythmusByte';
 let firstName = 'Akhil';
 let lastName = 'Mahesh';
 
+// Print
+
 console.log('My name is', firstName, lastName, '\nAlso known as', name);
+
+// Alert
+
+alert('Hey RhythmusByte')
